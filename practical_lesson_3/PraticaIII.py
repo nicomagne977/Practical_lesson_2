eb7ca47d1dde76dff588474c13aa23090253447c
andrgla <andrgla@stud.ntnu.no>
2025-09-18 13:59:36 -0300

    Added part 3 files

---
+++ practical_lesson_3/PraticaIII.py
@@ -0,0 +1,48 @@
+from PIL import Image
+from Cuif import Cuif
+import math
+
+def PSNR(original,decodificada,b):
+    try:
+        mse = MSE(original,decodificada) 
+        psnr = 10*math.log10(((2**b-1)**2)/mse)
+        return psnr
+    except ZeroDivisionError:
+        return "Infinito"
+
+def MSE(ori, dec):
+    nsymbols = ori.width * ori.height * 3
+    for i in range(ori.width):
+        for j in range(ori.height):
+            ori_r, ori_g, ori_b = ori.getpixel((i, j))
+            dec_r, dec_g, dec_b = dec.getpixel((i, j))
+    return 0
+
+if __name__ == "__main__":
+    filepath = 'lena.bmp'
+    img = Image.open(filepath)
+    matriculas = [20205642,2022423]
+    
+    # instancia objeto Cuif, convertendo imagem em CUIF.1
+    cuif = Cuif(img,1,matriculas)
+    
+    # imprime cabeçalho Cuif
+    cuif.printHeader()
+    
+    #gera o arquivo Cuif.1
+    cuif.save('lena1.cuif')
+    
+    #Abre um arquivo Cuif e gera o objeto Cuif
+    #cuif1 = Cuif.openCUIF('lena1.cuif')
+    
+    # Converte arquivo Cuif em BMP e mostra
+    #cuif1.saveBMP("lena1.bmp")
+    #cuif1.show()
+    #img1 = Image.open("lena1.bmp")
+
+    # Cálculo do PSNR
+    #psnr = PSNR(img, img1, 8)
+    #print(f'Cálculo do PSNR: {psnr}')
+
+  
+
--- praticaii.py
+++ praticaii.py
@@ -2,64 +2,6 @@ from urllib.request import urlopen
 from PIL import Image # package pillow
 import math
 
-def criarImagemRGB():
-    img = Image.new( "RGB", (512,256))
-    raster = img.load()
-        
-    for i in range(img.size[0]):
-        for j in range(img.size[1]):
-            raster[i,j] = (220,219,97,255)
-            
-    # obtendo o pixel 0,0
-    (r, g, b) = img.getpixel((0, 0))
-    print("Pixel (0,0) com getpixel:", r, g, b)
-    
-    # outra forma
-    print("Pixel (0,0): com raster", raster[0, 0])
-    
-    return img
-
-def criarImagemCinza():
-    img = Image.new( "L", (256,256))
-    raster = img.load()
-    for i in range(img.size[0]):
-        for j in range(img.size[1]):
-            raster[i,j] = i
-    y = img.getpixel((5, 5))
-    print(y)
-    return img
-
-def criarImagemBinaria():
-    # checkerboard pattern.
-    img = Image.new("1", (500,500))
-    raster = img.load()
-    for i in range(img.size[0]):
-        for j in range(img.size[1]):
-            if ((int(i/50)+int(j/50)) % 2 == 0):
-                raster[i,j] = 0
-            else:
-                raster[i,j] = 1
-    y = img.getpixel((0, 0))
-    print(y)
-    return img
-
-
-
-img = Image.open(urlopen("https://www.inf.ufsc.br/~roberto.willrich/INE5431/RGB.png"))
-print("Largura:", img.width, "Altura:", img.height)
-img.show()
-criarImagemRGB().show()
-criarImagemCinza().show()
-criarImagemBinaria().show()
-reduceResolution(img).show()
-grayScale(img).show()
-blackAndWhite(img).show()
-
-# Task 4: Reduce image to 3 bits per pixel
-print("Task 4: Reducing image to 3 bits per pixel...")
-img_3bits = reduzirPara3Bits(img)
-img_3bits.show()
-
 # Task 1 -> Reduce the resolution of the image to half the height 
 # and width of the  rgb.png  image and display this image.
 def reduceResolution(img):
@@ -137,5 +79,65 @@ def reduzirPara3Bits(img):
 # shade of red equal to the R component of the pixels (i, j) in the original image. Likewise, 
 # images G and B should contain the green and blue intensities of the pixels in the original image.
 
+def criarImagemRGB():
+    img = Image.new( "RGB", (512,256))
+    raster = img.load()
+        
+    for i in range(img.size[0]):
+        for j in range(img.size[1]):
+            raster[i,j] = (220,219,97,255)
+            
+    # obtendo o pixel 0,0
+    (r, g, b) = img.getpixel((0, 0))
+    print("Pixel (0,0) com getpixel:", r, g, b)
+    
+    # outra forma
+    print("Pixel (0,0): com raster", raster[0, 0])
+    
+    return img
 
+def criarImagemCinza():
+    img = Image.new( "L", (256,256))
+    raster = img.load()
+    for i in range(img.size[0]):
+        for j in range(img.size[1]):
+            raster[i,j] = i
+    y = img.getpixel((5, 5))
+    print(y)
+    return img
 
+def criarImagemBinaria():
+    # checkerboard pattern.
+    img = Image.new("1", (500,500))
+    raster = img.load()
+    for i in range(img.size[0]):
+        for j in range(img.size[1]):
+            if ((int(i/50)+int(j/50)) % 2 == 0):
+                raster[i,j] = 0
+            else:
+                raster[i,j] = 1
+    y = img.getpixel((0, 0))
+    print(y)
+    return img
+
+# Main execution
+img = Image.open(urlopen("https://www.inf.ufsc.br/~roberto.willrich/INE5431/RGB.png"))
+print("Largura:", img.width, "Altura:", img.height)
+img.show()
+criarImagemRGB().show()
+criarImagemCinza().show()
+criarImagemBinaria().show()
+
+# Task 1: Reduce resolution
+reduceResolution(img).show()
+
+# Task 2: Convert to grayscale
+grayScale(img).show()
+
+# Task 3: Convert to binary
+blackAndWhite(img).show()
+
+# Task 4: Reduce to 3 bits per pixel
+print("Task 4: Reducing image to 3 bits per pixel...")
+img_3bits = reduzirPara3Bits(img)
+img_3bits.show()
